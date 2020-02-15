7112648491b676908210d7a12ff682b5019f4b14
ArturZybin <artur.zybin.work@gmail.com>
2020-02-16 01:20:55 +0700

    add tasks sorting
    undone before done, then in order of creating

--- src/jsComponents/TaskList.js
+++ src/jsComponents/TaskList.js
@@ -5,7 +5,7 @@ import { Task } from './Task.js'
 
 class TaskList extends React.Component {
   renderTasks = () => {
-    const tasks = this.props.data;
+    const tasks = this.sortTasks(this.props.data);
     let tasksListTemplate = [];
     
     tasksListTemplate = tasks.map((item) => {
@@ -21,6 +21,19 @@ class TaskList extends React.Component {
     })
     
     return tasksListTemplate;
+  }
+  
+  sortTasks = (tasks) => {
+    const doneTasks = tasks.filter((item) => item.done);
+    const undoneTasks = tasks.filter((item) => !item.done);
+    
+    doneTasks.sort( (item1, item2) => (item1.id > item2.id ? 1 : -1) )
+    undoneTasks.sort( (item1, item2) => (item1.id > item2.id ? 1 : -1) )
+    
+    doneTasks.reverse();
+    undoneTasks.reverse();
+    
+    return undoneTasks.concat(doneTasks)
   }
   
   render() {

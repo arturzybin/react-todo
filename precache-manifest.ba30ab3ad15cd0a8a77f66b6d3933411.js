59fab21951e257eaa4e0d020dcb7391f8a82507a
ArturZybin <artur.zybin.work@gmail.com>
2020-02-16 13:43:44 +0700

    Updates

---
+++ precache-manifest.ba30ab3ad15cd0a8a77f66b6d3933411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2a97740a4fcde3af848524b31efa81",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "0c375403b581cb371b58",
+    "url": "/react-todo/static/css/main.fbf15f99.chunk.css"
+  },
+  {
+    "revision": "8ce366b3eb4a0823bef3",
+    "url": "/react-todo/static/js/2.e32ea0f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todo/static/js/2.e32ea0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c375403b581cb371b58",
+    "url": "/react-todo/static/js/main.e2a548ab.chunk.js"
+  },
+  {
+    "revision": "4637a77acbbb1bfa3441",
+    "url": "/react-todo/static/js/runtime-main.852ef8c9.js"
+  }
+]);

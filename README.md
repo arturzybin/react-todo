3ce4c477f7bd476b4c4def7023727989c4f8bbce
ArturZybin <artur.zybin.work@gmail.com>
2020-02-16 13:46:28 +0700

    update readme, rename '/docs' to '/build'

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 My first React app created without any tutorial, only my smart head =)
+
+Available at https://arturzybin.github.io/react-todo/
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ae8f0eb79204fe61b2c35d3b6a4b406.js"
+  "/react-todo/precache-manifest.ba30ab3ad15cd0a8a77f66b6d3933411.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

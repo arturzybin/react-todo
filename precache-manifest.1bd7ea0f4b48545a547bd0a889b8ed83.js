b68bdd4142a0737cd82d6c907d09c9bfd2eba12f
ArturZybin <artur.zybin.work@gmail.com>
2020-02-16 17:32:16 +0700

    Updates

--- precache-manifest.1bd7ea0f4b48545a547bd0a889b8ed83.js
+++ precache-manifest.1bd7ea0f4b48545a547bd0a889b8ed83.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e2a97740a4fcde3af848524b31efa81",
+    "revision": "00ff8a16337a26764cc0b18050512542",
     "url": "/react-todo/index.html"
   },
   {
-    "revision": "0c375403b581cb371b58",
+    "revision": "fa53c9a64091cf2d5bab",
     "url": "/react-todo/static/css/main.fbf15f99.chunk.css"
   },
   {
-    "revision": "8ce366b3eb4a0823bef3",
-    "url": "/react-todo/static/js/2.e32ea0f1.chunk.js"
+    "revision": "9b6a3d6ce56c20e89b29",
+    "url": "/react-todo/static/js/2.fad8f906.chunk.js"
   },
   {
     "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
-    "url": "/react-todo/static/js/2.e32ea0f1.chunk.js.LICENSE.txt"
+    "url": "/react-todo/static/js/2.fad8f906.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0c375403b581cb371b58",
-    "url": "/react-todo/static/js/main.e2a548ab.chunk.js"
+    "revision": "fa53c9a64091cf2d5bab",
+    "url": "/react-todo/static/js/main.71f3c50c.chunk.js"
   },
   {
     "revision": "4637a77acbbb1bfa3441",

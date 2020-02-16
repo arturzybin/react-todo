24990661f9c0812a6def75defb8ad2449bb357ca
ArturZybin <artur.zybin.work@gmail.com>
2020-02-16 13:17:34 +0700

    finally add build directory

---
+++ build/precache-manifest.7ae8f0eb79204fe61b2c35d3b6a4b406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab9af6684847410f24757e74d915c4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3364e4596be5f8827c3c",
+    "url": "/static/css/main.fbf15f99.chunk.css"
+  },
+  {
+    "revision": "8ce366b3eb4a0823bef3",
+    "url": "/static/js/2.e32ea0f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e32ea0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3364e4596be5f8827c3c",
+    "url": "/static/js/main.30425dbb.chunk.js"
+  },
+  {
+    "revision": "d84a3e19d8a947dff9dd",
+    "url": "/static/js/runtime-main.b2044548.js"
+  }
+]);
